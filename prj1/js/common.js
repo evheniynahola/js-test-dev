362fe3949bfffd9b1ff74c114249649f8337891f
Evheniy <evgeniy.nagola@gmail.com>
2020-05-19 21:18:06 +0300

    Первый коммит

---
+++ prj1/js/common.js
@@ -0,0 +1,54 @@
+'use strict'; // Более строгий код
+
+// var a = 1;
+// let b = 2;
+// const c =3;
+
+// let persone = {
+//     name: 'Jhon',
+//     age: 18,
+//     prof: 'engineer'
+// }
+
+// let arr = ['ph1.jpg', 'ph2.jpg', 'ph3.jpg'];
+
+
+// ВОПРОСЫ start
+
+//let answer, state;
+
+// answer = confirm('Are you here ?');
+
+//state = prompt('How are you ?', 'i am fine');
+
+//alert(typeof(state));
+
+// if(answer) {
+//     state = prompt('How are you ?', 'i am fine');
+// }
+
+// else {
+//     alert('bye');
+// }
+
+// ВОПРОСЫ end
+
+
+// Операторы start
+
+// let incr = 1, decr = 2;
+
+// console.log(++incr);
+// console.log(incr++);
+
+// console.log(5%2);
+
+//console.log('2' == 2); //true
+//console.log('2' === 2); //false
+
+// let checked = true, nonchecked = false;
+
+// console.log(checked && !nonchecked);
+
+// Операторы end
+
